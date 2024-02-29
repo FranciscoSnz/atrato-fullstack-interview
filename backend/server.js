772d5d6c51eab7ea60f292f67bf2411de3077ce5
FranciscoSnz <hss_francisco@hotmail.com>
2024-02-28 20:29:09 -0700

    Reto atrato francisco

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,97 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
+const userData = require("../data/data.json");
+const axios = require("axios");
+let userCount = 5;
+
+app.use(bodyParser.json());
 
 app.listen(5000, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
-app.get('/', function(req, res){
-    res.send('Hello from backend')    
-})
+app.get("/", function (req, res) {
+  res.send("Hello from backend");
+});
+
+app.get("/users", function (req, res) {
+  res.send(userData);
+});
+
+app.get("/users/:id", function (req, res) {
+  const userId = req.params.id;
+
+  const index = userData.findIndex((curr) => curr.id == userId);
+  if (index >= 0) {
+    res.send(userData[index]);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+async function getCardData() {
+  try {
+    const response = await axios.get(`https://randommer.io/api/Card`, {
+      headers: { "X-Api-Key": "f3b80c8d2c6a478e89445e919e625fff" },
+    });
+    return response.data;
+  } catch (exception) {
+    console.log(exception);
+    return null;
+  }
+}
+
+app.post("/users", async function (req, res) {
+  const body = req.body;
+  console.log(body);
+
+  body.id = userCount++;
+  body.status = "PENDIENTE";
+  body.assignedAnalyst = "David";
+
+  body.cardInfo = await getCardData();
+  if (body.cardInfo == null) {
+    res.sendStatus(500);
+    return;
+  }
+
+  delete body.cardInfo.fullName;
+
+  console.log("Saving user: ");
+  console.log(body);
+  userData.push(body);
+
+  res.status(201).send(body);
+});
+
+app.patch("/users/:id", function (req, res) {
+  const userId = req.params.id;
+  const body = req.body;
+  console.log(userId);
+  console.log(body);
+
+  // Asumiendo el usuario edita todo junto sin validar
+  const index = userData.findIndex((curr) => curr.id == userId);
+  if (index < 0) {
+    res.send(404);
+    return;
+  }
+
+  userData[index] = body;
+
+  res.status(200).send(userData[index]);
+});
+
+app.delete("/users/:id", function (req, res) {
+  const userId = req.params.id;
+  console.log(userId);
+  const index = userData.findIndex((curr) => curr.id == userId);
+  if (index >= 0) {
+    userData.splice(index, 1);
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+function UserCard({ user }) {
+  return (
+    <div>
+      <h2>
+        {user.name} {user.fLastName} {user.sLastName}
+      </h2>
+      <p>ID: {user.id}</p>
+      <p>Mail: {user.email}</p>
+      <p>Fecha de Nacimiento: {user.birthday}</p>
+      <p>Telefono: {user.phone}</p>
+      <p>Analista Asignado: {user.assignedAnalyst}</p>
+      <p>Tarjeta: {user.cardInfo.number}</p>
+    </div>
+  );
+}
 
 function App() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/users").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  if (!users) return "Loading...";
+
+  const usersList = users.map((user) => {
+    return <UserCard key={user.id} user={user} />;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Atrato App</h1>
+      {usersList}
     </div>
   );
 }
